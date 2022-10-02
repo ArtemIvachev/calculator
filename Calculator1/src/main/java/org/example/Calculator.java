8d91cca5e7fb8c05f19075e09bf34a92d0831354
Артем <95567445+ArtemIvachev@users.noreply.github.com>
2022-10-02 16:34:13 +0300

    Update Calculator.java

--- Calculator1/src/main/java/org/example/Calculator.java
+++ Calculator1/src/main/java/org/example/Calculator.java
@@ -2,18 +2,21 @@ package org.example;
 
 public class Calculator {
 
-    public int getSum(int x, int y) {
+    public static int getSum(int x, int y) {
         return x+y;
     }
 
-    public int getDivide(int x, int y) {
+    public static int getDivide(int x, int y) {
+        if (y == 0) {
+            throw new IllegalArgumentException("Number can't be divide by 0!");
+        }
         return x/y;
     }
 
-    public int getMultiple(int x, int y) {
+    public static int getMultiple(int x, int y) {
         return x*y;
     }
-    public int getSubtract(int x, int y) {
+    public static int getSubtract(int x, int y) {
         return x-y;
     }
 }
